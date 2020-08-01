bc6c4b9cc79c9f21f093243613e8953c61b11723
SwapnilFadte <swapnil.fadte@unigoa.ac.in>
2020-08-01 16:44:30 +0530

    page structure

--- page_structure_content.php
+++ page_structure_content.php
@@ -1,13 +1,29 @@
 
 <?php
 
-$header="<h2>COVID Goa Data Analysis</h2>
-  <h4>Visualisation and prediction</h4>";  
+$header = "<h2>COVID Goa Data Analysis</h2>
+  <h4>Visualisation and prediction</h4>";
 
-$navigation_bar="<a class=\"active\" href=\"index.php\">Home</a>
-  <a href=\"paitent_data_upload.php\"> Upload Mater Tables </a>
-  <a href=\"electricity_data_upload.php\">Report Downloads </a>";
+$navigation_bar = "<a class=\"active\" href=\"index.php\">Home</a>
+  <a href=\"uploadMasterTable.php\"> Upload Mater Tables </a>
+  <a href=\"downloadReports.php\">Report Downloads </a>";
 
-$footer="<p>contact details: abc@nn.cc</p>";
 
+$masterTabelupload = "<h2>Upload Master Tables</h2>
+            <ul>
+                <li><a href=paitent_data_upload.php>Upload Patient Data </a></li>
+                <li><a href=electricity_data_upload.php>Electricity data Upload</a> </li>
+                <li><a href=covid_care_bed_data_upload.php> COVID Care Center Bed data Upload</a> </li>
+                <li><a href=covid_hospital_data_upload.php>COVID Hospital data Upload</a> </li>
+                <li><a href=covid_care_center_data_upload.php>COVID Care Center data Upload</a> </li>
+            </ul>";
+
+$downloadReport = " <h2>Downlod Report</h2>
+            <ul>
+                <li><a href=downloadContactTracingReport.php>Downlod ContactTracing Report</a></li>
+                <li><a href=downloadCovidCenterBedStatusReport.php>Downlod COVID Center Bed Status Report</a></li>
+                <li><a href=downloadHospitalCenterBedStatusReport.php>Downlod COVID Hospital Bed Status Report</a></li>
+            </ul> ";
+
+$footer = "<p>contact details: abc@nn.cc</p>";
 ?>
