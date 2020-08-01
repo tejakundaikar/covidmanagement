f54379f083a38d71e3911cc29efe930c48e9f7bf
teja <tkundaikar10@gmail.com>
2020-08-01 20:50:46 +0530

    edited file

--- downloadCovidCenterBedStatusReport.php
+++ downloadCovidCenterBedStatusReport.php
@@ -5,7 +5,7 @@ require_once 'DataSource.php';
 $db = new DataSource();
 $conn = $db->getConnection();
 
-$filename = "COVID_HOSPITAL_BED_STATUS".date("Y-m-d").".csv";
+$filename = "COVID_CENTER_BED_STATUS".date("Y-m-d").".csv";
 $fp = fopen('php://output', 'w');
 
 $arr = array("Sr.No", 
@@ -26,7 +26,7 @@ foreach ($arr as &$value) {
     $header[]= $value;
 }
 
-$arr2=array("","HOSPITAL BED STATUS", "","Date");
+$arr2=array("","COVID CENTER BED STATUS", "","Date");
 
 array_push($arr2,date("Y-m-d"));
 foreach ($arr2 as &$value2) {
@@ -45,17 +45,17 @@ fputcsv($fp, $blank);
 fputcsv($fp, $commanheader);
 fputcsv($fp, $header);
 fputcsv($fp, $symbol);
-$query = "select h.ch_id, h.ch_name,  
+$query = "select h.cc_id, h.cc_name,  
 c.male_beds, c.female_beds, c.emergency_beds, c.male_beds+c.female_beds+c.emergency_beds as totalbed,
 c.male_beds-o.male_occupied as malebedavail, c.female_beds-o.female_occupied as femalebedsavail , c.emergency_beds-o.emergency_occupied as emergencybedavail, c.emergency_beds-o.emergency_occupied+ c.female_beds-o.female_occupied +  c.male_beds-o.male_occupied as totalbedavail,
 o.male_likelyfree, o.female_likelyfree, o.emergency_likelyfree, 
- o.date_updated from CovidHospital h, HSBedCapacity c, HSBedOccupancy o where h.ch_id=c.ch_id and c.ch_id=o.ch_id";
+ o.date_updated from CovidCareCenter h, CCBedCapacity c, CCBedOccupancy o where h.cc_id=c.cc_id and c.cc_id=o.cc_id";
 
 $result = mysqli_query($conn, $query);
 while($row = mysqli_fetch_row($result)) {
 	fputcsv($fp, $row);
 }
-$query1 = "select sum(c.male_beds), sum(c.female_beds), sum(c.emergency_beds), sum(c.male_beds+c.female_beds+c.emergency_beds), sum(c.male_beds-o.male_occupied) as malebedavail, sum(c.female_beds-o.female_occupied) as femalebedsavail , sum(c.emergency_beds-o.emergency_occupied) as emergencybedavail, sum(c.emergency_beds-o.emergency_occupied+ c.female_beds-o.female_occupied +  c.male_beds-o.male_occupied) as totalbedavail from CovidHospital h, HSBedCapacity c, HSBedOccupancy o where h.ch_id=c.ch_id and c.ch_id=o.ch_id";
+$query1 = "select sum(c.male_beds), sum(c.female_beds), sum(c.emergency_beds), sum(c.male_beds+c.female_beds+c.emergency_beds), sum(c.male_beds-o.male_occupied) as malebedavail, sum(c.female_beds-o.female_occupied) as femalebedsavail , sum(c.emergency_beds-o.emergency_occupied) as emergencybedavail, sum(c.emergency_beds-o.emergency_occupied+ c.female_beds-o.female_occupied +  c.male_beds-o.male_occupied) as totalbedavail from CovidCareCenter h, CCBedCapacity c, CCBedOccupancy o where h.cc_id=c.cc_id and c.cc_id=o.cc_id";
 
 
 $result = mysqli_query($conn, $query1);
